a7be8bb4751dc27f9eb80a512d436b3a4160b02c
chonchol <chonchol57@gmail.com>
2024-10-19 18:50:42 +0600

    fix nav menu and formatiing file

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,79 @@
-let API_URL = 'https://gutendex.com/books';
+let API_URL = "https://gutendex.com/books";
 let currentPage = 1;
 let booksData = [];
 let genres = [];
 
-const prevBtn = document.getElementById('prev-btn');
-const nextBtn = document.getElementById('next-btn');
-const currentPageSpan = document.getElementById('current-page');
-const loadingElement = document.getElementById('loading');
-const searchField = document.getElementById('search-field');
-const genreFilter = document.getElementById('genre-filter');
-const paginationDiv = document.getElementById('pagination');
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const currentPageSpan = document.getElementById("current-page");
+const loadingElement = document.getElementById("loading");
+const searchField = document.getElementById("search-field");
+const genreFilter = document.getElementById("genre-filter");
+const paginationDiv = document.getElementById("pagination");
 
 async function fetchBookList(url) {
-    try {
-        loadingElement.style.display = 'block'; 
-        paginationDiv.style.display = 'none'; 
-        const response = await fetch(url);
+  try {
+    loadingElement.style.display = "block";
+    paginationDiv.style.display = "none";
+    const response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        booksData = data.results;
-        // console.log("Books data fetched:", booksData);
-
-        displayBooks(booksData);
-        genreList(booksData);
-        updatePaginationButtons(data.previous, data.next);
-    } catch (error) {
-        console.error('There was a problem fetching the book list:', error);
-    } finally {
-        loadingElement.style.display = 'none'; 
-        paginationDiv.style.display = 'inherit'; 
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+
+    const data = await response.json();
+    booksData = data.results;
+    // console.log("Books data fetched:", booksData);
+
+    displayBooks(booksData);
+    genreList(booksData);
+    updatePaginationButtons(data.previous, data.next);
+  } catch (error) {
+    console.error("There was a problem fetching the book list:", error);
+  } finally {
+    loadingElement.style.display = "none";
+    paginationDiv.style.display = "inherit";
+  }
 }
 
 function displayBooks(books) {
-    const container = document.getElementById('books-container');
-    container.innerHTML = '';
+  const container = document.getElementById("books-container");
+  container.innerHTML = "";
 
-    if (books.length === 0) {
-        container.innerHTML = '<p class="text-center p-5 mx-auto w-full text-2xl">No books found with the given Title!</p>'; 
-        return;
-    }
+  if (books.length === 0) {
+    container.innerHTML =
+      '<p class="text-center p-5 mx-auto w-full text-2xl">No books found with the given Title!</p>';
+    return;
+  }
 
-    books.forEach(book => {
-        const author = book.authors.length > 0 ? book.authors[0].name : 'Unknown author';
-        const subjects = book.subjects.join(', ') || 'No subjects listed';
-        const coverImage = book.formats['image/jpeg'] || 'https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg';
-        const bookID = book.id;
+  books.forEach((book) => {
+    const author =
+      book.authors.length > 0 ? book.authors[0].name : "Unknown author";
+    const subjects = book.subjects.join(", ") || "No subjects listed";
+    const coverImage =
+      book.formats["image/jpeg"] ||
+      "https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg";
+    const bookID = book.id;
 
-        const savedBooks = JSON.parse(localStorage.getItem('wishlistedBooks')) || [];
-        const isWishlisted = savedBooks.some(savedBook => savedBook.id === bookID);
+    const savedBooks =
+      JSON.parse(localStorage.getItem("wishlistedBooks")) || [];
+    const isWishlisted = savedBooks.some(
+      (savedBook) => savedBook.id === bookID
+    );
 
-        const bookDiv = document.createElement('div');
-        bookDiv.classList.add('w-full', 'md:w-1/3', 'xl:w-1/4', 'p-0','md:p-6', 'flex', 'flex-col', 'py-6');
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add(
+      "w-full",
+      "md:w-1/3",
+      "xl:w-1/4",
+      "p-0",
+      "md:p-6",
+      "flex",
+      "flex-col",
+      "py-6"
+    );
 
-        bookDiv.innerHTML = `
+    bookDiv.innerHTML = `
         <div>
             <div id="cover-img" class="cover-img w-full h-[300px]">
             <a href="book-details.html" data-book-id="${bookID}">
@@ -66,9 +82,13 @@ function displayBooks(books) {
             </a>
             </div>
             <div class="pt-5 flex items-start justify-between w-full">
-                <a href="book-details.html" class="font-bold hover:text-black text-[18px] w-4/5 book-title" data-book-id="${bookID}">${book.title}</a>
+                <a href="book-details.html" class="font-bold hover:text-black text-[18px] w-4/5 book-title" data-book-id="${bookID}">${
+      book.title
+    }</a>
                 <div class="flex justify-end w-1/5 mt-1">
-                    <img class="w-5 h-5 cursor-pointer love-icon" data-book-id="${bookID}" src="${isWishlisted ? `images/heart-solid.svg` : `images/heart-regular.svg`} " alt="">
+                    <img class="w-5 h-5 cursor-pointer love-icon" data-book-id="${bookID}" src="${
+      isWishlisted ? `images/heart-solid.svg` : `images/heart-regular.svg`
+    } " alt="">
                 </div>
             </div>
             <p id="author-name" class="author-name text-sm">Written by: ${author}</p>
@@ -79,153 +99,149 @@ function displayBooks(books) {
         </div>
         `;
 
-        container.appendChild(bookDiv);
-    });
+    container.appendChild(bookDiv);
+  });
 
-    document.querySelectorAll('.book-title').forEach(title => {
-        title.addEventListener('click', function(event) {
-            const bookID = event.target.getAttribute('data-book-id');
-            const selectedBook = books.find(book => book.id == bookID);
+  document.querySelectorAll(".book-title").forEach((title) => {
+    title.addEventListener("click", function (event) {
+      const bookID = event.target.getAttribute("data-book-id");
+      const selectedBook = books.find((book) => book.id == bookID);
 
-            if (selectedBook) {
-                localStorage.setItem('selectedBook', JSON.stringify(selectedBook));
-                window.location.href = 'book-details.html';
-            }
-        });
+      if (selectedBook) {
+        localStorage.setItem("selectedBook", JSON.stringify(selectedBook));
+        window.location.href = "book-details.html";
+      }
     });
+  });
 
-    document.querySelectorAll('.love-icon').forEach(icon => {
-        icon.addEventListener('click', toggleWishlist);
-    });
+  document.querySelectorAll(".love-icon").forEach((icon) => {
+    icon.addEventListener("click", toggleWishlist);
+  });
 }
 
 function toggleWishlist(event) {
-    const bookID = event.target.getAttribute('data-book-id');
+  const bookID = event.target.getAttribute("data-book-id");
 
-    console.log(`Toggling wishlist for book ID: ${bookID}`); 
+  console.log(`Toggling wishlist for book ID: ${bookID}`);
 
-    let savedBooks = JSON.parse(localStorage.getItem('wishlistedBooks')) || [];
-    const bookIndex = savedBooks.findIndex(book => book.id == bookID);
+  let savedBooks = JSON.parse(localStorage.getItem("wishlistedBooks")) || [];
+  const bookIndex = savedBooks.findIndex((book) => book.id == bookID);
 
-    if (bookIndex > -1) {
-        savedBooks.splice(bookIndex, 1);
-        event.target.src = 'images/heart-regular.svg'; 
-    } else {
-        const bookToWishlist = booksData.find(book => book.id == bookID);
-        
-        if (!bookToWishlist) {
-            console.error(`Book with ID ${bookID} not found in booksData`);
-            return;
-        }
+  if (bookIndex > -1) {
+    savedBooks.splice(bookIndex, 1);
+    event.target.src = "images/heart-regular.svg";
+  } else {
+    const bookToWishlist = booksData.find((book) => book.id == bookID);
 
-        savedBooks.push(bookToWishlist);
-        event.target.src = 'images/heart-solid.svg';
+    if (!bookToWishlist) {
+      console.error(`Book with ID ${bookID} not found in booksData`);
+      return;
     }
 
-    localStorage.setItem('wishlistedBooks', JSON.stringify(savedBooks));
+    savedBooks.push(bookToWishlist);
+    event.target.src = "images/heart-solid.svg";
+  }
+
+  localStorage.setItem("wishlistedBooks", JSON.stringify(savedBooks));
 }
 
-function genreList(books){
-    const uniqueGenres = new Set();
+function genreList(books) {
+  const uniqueGenres = new Set();
 
-    books.forEach( book => {
-        book.subjects.forEach(subject => 
-            uniqueGenres.add(subject)
-        );
-    });
+  books.forEach((book) => {
+    book.subjects.forEach((subject) => uniqueGenres.add(subject));
+  });
 
-    genreFilter.innerHTML = '<option value="">Select a genre</option>';
+  genreFilter.innerHTML = '<option value="">Select a genre</option>';
 
-    uniqueGenres.forEach(genre => {
-        const option = document.createElement('option');
-        option.value = genre;
-        option.textContent = genre;
-        genreFilter.appendChild(option);
-    });
+  uniqueGenres.forEach((genre) => {
+    const option = document.createElement("option");
+    option.value = genre;
+    option.textContent = genre;
+    genreFilter.appendChild(option);
+  });
 }
 
-function filterBooksByGenre(genre){
-    if (genre === ""){
-        displayBooks(booksData);
-    } else {
-        const filteredBooks = booksData.filter(book => 
-            book.subjects.includes(genre)
-        );
-        displayBooks(filteredBooks);
-    }
+function filterBooksByGenre(genre) {
+  if (genre === "") {
+    displayBooks(booksData);
+  } else {
+    const filteredBooks = booksData.filter((book) =>
+      book.subjects.includes(genre)
+    );
+    displayBooks(filteredBooks);
+  }
 }
 
 function updatePaginationButtons(previous, next) {
-    if (previous) {
-        prevBtn.disabled = false;
-        prevBtn.setAttribute('data-url', previous);
-    } else {
-        prevBtn.disabled = true;
-        prevBtn.style.cursor = 'not-allowed';
-    }
-    if (next) {
-        nextBtn.disabled = false;
-        nextBtn.setAttribute('data-url', next);
-    } else {
-        nextBtn.disabled = true;
-        nextBtn.style.cursor = 'not-allowed';
-    }
-    currentPageSpan.textContent = `Page: ${currentPage}`;
+  if (previous) {
+    prevBtn.disabled = false;
+    prevBtn.setAttribute("data-url", previous);
+  } else {
+    prevBtn.disabled = true;
+    prevBtn.style.cursor = "not-allowed";
+  }
+  if (next) {
+    nextBtn.disabled = false;
+    nextBtn.setAttribute("data-url", next);
+  } else {
+    nextBtn.disabled = true;
+    nextBtn.style.cursor = "not-allowed";
+  }
+  currentPageSpan.textContent = `Page: ${currentPage}`;
 }
 
 // Add event listeners for pagination buttons after the DOM is loaded
-prevBtn.addEventListener('click', () => {
-    const prevUrl = prevBtn.getAttribute('data-url');
-    if (prevUrl) {
-        currentPage--;
-        fetchBookList(prevUrl);
+prevBtn.addEventListener("click", () => {
+  const prevUrl = prevBtn.getAttribute("data-url");
+  if (prevUrl) {
+    currentPage--;
+    fetchBookList(prevUrl);
+  }
+});
+
+nextBtn.addEventListener("click", () => {
+  const nextUrl = nextBtn.getAttribute("data-url");
+  if (nextUrl) {
+    currentPage++;
+    fetchBookList(nextUrl);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedSearchTerm = localStorage.getItem("searchTerm");
+  const container = document.getElementById("books-container");
+
+  container.style.display = "none";
+
+  fetchBookList(API_URL).then(() => {
+    if (savedSearchTerm) {
+      searchField.value = savedSearchTerm;
+
+      const filteredBooks = booksData.filter((book) =>
+        book.title.toLowerCase().includes(savedSearchTerm)
+      );
+      displayBooks(filteredBooks);
+    } else {
+      displayBooks(booksData);
     }
+    container.style.display = "inherit";
+  });
 });
 
-nextBtn.addEventListener('click', () => {
-    const nextUrl = nextBtn.getAttribute('data-url');
-    if (nextUrl) {
-        currentPage++;
-        fetchBookList(nextUrl);
-    }
+searchField.addEventListener("input", (e) => {
+  const searchTerm = e.target.value.toLowerCase();
+  localStorage.setItem("searchTerm", searchTerm);
+  const filteredBooks = booksData.filter((book) =>
+    book.title.toLowerCase().includes(searchTerm)
+  );
+  displayBooks(filteredBooks);
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const savedSearchTerm = localStorage.getItem('searchTerm');
-    const container = document.getElementById('books-container');
-
-    container.style.display = 'none';
-
-    fetchBookList(API_URL).then(() => {
-        if (savedSearchTerm) {
-            searchField.value = savedSearchTerm; 
-
-            const filteredBooks = booksData.filter(book =>
-                book.title.toLowerCase().includes(savedSearchTerm)
-            );
-            displayBooks(filteredBooks); 
-        } else {
-            displayBooks(booksData); 
-        }
-        container.style.display = 'inherit';
-    });
+genreFilter.addEventListener("change", (e) => {
+  const selectGenre = e.target.value;
+  filterBooksByGenre(selectGenre);
 });
-
-
-searchField.addEventListener('input', (e) => {
-    const searchTerm = e.target.value.toLowerCase();
-    localStorage.setItem('searchTerm', searchTerm);
-    const filteredBooks = booksData.filter(book =>
-        book.title.toLowerCase().includes(searchTerm)
-    );
-    displayBooks(filteredBooks);
-});
-
-genreFilter.addEventListener('change', (e) => {
-    const selectGenre = e.target.value;
-    filterBooksByGenre(selectGenre);
-})
-
 
 // Fetch the initial book list
 fetchBookList(API_URL);
